62638cfbd2d92eacb6eaf3e3954897ce16183b5c
timriffe <tim.riffe@gmail.com>
2025-03-11 11:57:28 +0100

    adds HLE calculations and links to Nusselder& Looman(2003) for challenge

--- 03_sullivan.R
+++ 03_sullivan.R
@@ -12,7 +12,48 @@
 
 # 1.2 group lifetables to 2-year age groups (just need Lx, but sure to make lt start at 50!)
 
+
+lt_sullivan <-
+  lifetables |> 
+  filter(age >= 50) |> 
+  select(sex, time, age, lx, Lx) |> 
+  group_by(time, sex) |> 
+  mutate(Lx = Lx / lx[age == 50],
+         age = age - age %% 2) |> 
+  group_by(time, sex, age) |> 
+  summarize(Lx = sum(Lx), .groups = "drop")
+
+
+
 # 2 join prev and lifetable (either single or 2-yr ages)
+prev <- 
+  prev |> 
+  complete(condition, sex, time, age, 
+           fill = list(prev = 0))
+prev |> 
+  left_join(lt_sullivan, by = join_by(sex, time, age)) |> 
+  group_by(sex, time, condition) |> 
+  summarize(HLE = sum(Lx * (1 - prev)),
+            ULE = sum(Lx * prev),
+            LE = sum(Lx)) |> 
+  filter(condition != "chron") |> 
+  mutate(compression = ULE / LE) |> 
+  ggplot(aes(x= time, y = compression, color = sex)) +
+  geom_line()+
+  facet_wrap(~ condition)
+
+# equivalent
+# prev |> 
+#   left_join(lt_sullivan, by = c("sex", "time", "age")) 
+
+# Challenge:
+
+# Use the Nusselder-Looman decomposition method
+#https://www.proquest.com/docview/222945248?pq-origsite=gscholar&fromopenview=true&sourcetype=Scholarly%20Journals
+# (expand the box to see the formulas)
+
+# https://doi.org/10.1353/dem.2004.0017
+
 
 
 
