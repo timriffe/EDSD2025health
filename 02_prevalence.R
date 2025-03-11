d7e52a8761fa8f9a61eb0d08ef7b5da0c6a3a267
timriffe <tim.riffe@gmail.com>
2025-03-11 08:05:10 +0100

    more skelleton scripts

--- 02_prevalence.R
+++ 02_prevalence.R
@@ -18,7 +18,18 @@ library(tidyverse)
 
 
 
+#Self related:
+#  PH002_ HEALTH IN GENERAL QUESTION  V 1 | Would you say your health is ... | 1. Very good | 2. Good | 3. Fair | 4. Bad | 5. Very bad
+#Chronic:
+#  PH004_ LONG-TERM ILLNESS Some people suffer from chronic or long-term health problems. By  long-term we mean it has troubled you over a period of time or is  likely to affect you over a period of time. Do you have any long-term  health problems, illness, disability or infirmity? IWER: INCLUDING MENTAL HEALTH PROBLEMS 1. Yes 5. No 
+#GALI:
+#  PH005_ LIMITED ACTIVITIES For the past six months at least, to what extent have you been  limited because of a health problem in activities people usually do? IWER: READ OUT 1. Severely limited 2. Limited, but not severely 3. Not limited
+#for ADL and IADL:
+#  PH049_ MORE HEALTH AND ACTIVITIES Please look at card 10.Here are a few more everyday activities.  Please tell me if you have any difficulty with these because of a  physical, mental, emotional or memory problem. Again exclude any  difficulties you expect to last less than three months.(Because of a  health or memory problem, do you have difficulty doing any of the  activities on card 10?) IWER: PROBE: ANY OTHERS? CODE ALL THAT APPLY 1. Dressing, including putting on shoes and socks 2. Walking across a room 3. Bathing or showering 4. Eating, such as cutting up your food 5. Getting in or out of bed 6. Using the toilet, including getting up or down 7. Using a map to figure out how to get around in a strange place 8. Preparing a hot meal 9. Shopping for groceries 16 10. Making telephone calls 11. Taking medications 12. Doing work around the house or garden 13. Managing money, such as paying bills and keeping track of  expenses 96. None of these for IADL:
 
-
-
+# explanations:
+# for gali 0 is healthy otherwise U
+# for self 1 2 3 is H, others U
+# for chronic 0 is H otherwise U
+# for ADL and IADL anything more than 0 is U
 
---
+++ 04_calculate_transitions.R
@@ -0,0 +1,16 @@
+
+library(tidyverse)
+
+# 1 read in counts.csv.gz usig read_csv()
+
+# 2 calculate denominators by summing transitions
+# originating from a state and age (these are exhaustive)
+
+# 3 calculate transitions
+
+# 4 make some exploratory plots, try to understand what the
+# transitions mean. These could be noisy, so maybe geom_point() + geom_smooth() would help.
+
+# 5 if you wanted to calculate interesting ratios,
+# like UD / HD, then you'd probably want smoothed or modeled
+# probabilities
