e3fd933c1743cdd6f24f9969e95ddf12b5220683
timriffe <tim.riffe@gmail.com>
2025-03-11 07:36:06 +0100

    sets up container scripts for session

---
+++ 02_prevalence.R
@@ -0,0 +1,24 @@
+
+
+library(tidyverse)
+# 1 read in data/prev.czv.gz using read_csv()
+
+# 2 reshape so that states H and U are side-by-side, with values n
+# use pivot_wider() for this
+
+# 3 calculate prev as U / (U + H) using mutate()
+
+# 4 make some plots of prevalence, and some observations
+# would you maybe want to smooth this?
+# any odd features?
+
+# 5 make one or more plots of sex ratios in prevalence
+# what can be said about these?
+
+
+
+
+
+
+
+
---
+++ 03_sullivan.R
@@ -0,0 +1,20 @@
+
+# refer to lifetables calculated in 01 and prevalence in 02
+
+# 1 notice that prevalence is in 2-year age groups,
+# but lifetables are in single years.
+# to join on age, you need like age groups
+# either you gradaute prevalence (possibly smoothing at same time),
+# or you group lifetables to 2-year age groups. What'll it be
+
+
+# 1.1 smooth/graduate prevalence
+
+# 1.2 group lifetables to 2-year age groups (just need Lx, but sure to make lt start at 50!)
+
+# 2 join prev and lifetable (either single or 2-yr ages)
+
+
+
+
+
