02df4cc128ae5b65b806b174a6562478d951affa
timriffe <tim.riffe@gmail.com>
2025-03-13 09:58:42 +0100

    decomposition script

--- 11_decompose_ms.R
+++ 11_decompose_ms.R
@@ -13,7 +13,17 @@ tr <- read_csv("https://github.com/timriffe/EDSD2025health/raw/refs/heads/master
 source("https://raw.githubusercontent.com/timriffe/ms_sensitivity/refs/heads/master/R/00_functions_classic.R")
 source("https://raw.githubusercontent.com/timriffe/ms_sensitivity/refs/heads/master/R/00_sensitivity_functions.R")
 
-
+tr |> 
+  rename(p = p_avg) |> 
+  s2t(init = c(H=1,U=0), expectancy = "h") |> 
+  filter(transition != "init") |> 
+  mutate(age = age + 20) |> 
+  left_join(tr, by = join_by(age, transition)) |> 
+  mutate(dec = effect * delta) |> 
+  ggplot(aes(x = age, y = dec, color = transition)) +
+  geom_line()
+  # group_by(transition) |> 
+  # summarize(dec = sum(dec, na.rm = TRUE))
 
 
 
